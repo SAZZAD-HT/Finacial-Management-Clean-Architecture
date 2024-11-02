a200bfd82e50e8e2637b02a57fce55ed1d658162
sazzad <sazzadaiub1@gmail.com>
2024-11-02 12:02:25 +0600

    Add project files.

---
+++ Infrastructure/Repository/FinRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Interfaces;
+using Infrastructure.EF;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public  class FinRepository: IFinRepository
+    {
+        private readonly FinManagementEntities _context;
+
+        public FinRepository(FinManagementEntities context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable< User>>GetUser()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
